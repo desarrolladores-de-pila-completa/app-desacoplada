17787db7f14a415a1b335a31ac78ac7c13d97351
diego <63115070+zarkius@users.noreply.github.com>
2025-10-07 12:59:20 +0200

    Refactor code structure and remove redundant changes

--- react-native/mobile/android/app/build/intermediates/cxx/Debug/31b2w235/logs/arm64-v8a/build_command_targets.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-"C:\\Users\\diego\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\NAMESPACE\\app-desacoplada\\react-native\\mobile\\android\\app\\.cxx\\Debug\\31b2w235\\arm64-v8a" ^
-  appmodules ^
-  react_codegen_rnscreens ^
-  react_codegen_safeareacontext
