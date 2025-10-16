58e7078d315faad7c6e5e751eb0d1c922f02aa49
diego <63115070+zarkius@users.noreply.github.com>
2025-10-16 21:14:41 +0200

    Update plugin-bootstrap-colorpicker.js with new ColorInput code

--- frontend/src/libs/VvvebJs-master/libs/builder/plugin-bootstrap-colorpicker.js
+++ frontend/src/libs/VvvebJs-master/libs/builder/plugin-bootstrap-colorpicker.js
@@ -1,20 +1,20 @@
-let ColorInput = { ...ColorInput, ...{
+let ColorInput = Object.assign({}, typeof ColorInput !== 'undefined' ? ColorInput : {}, {
+  events: [
+    ["change", "onChange", "input"],
+  ],
 
-    events: [
-        ["change", "onChange", "input"],
-	 ],
+  setValue: function(value) {
+    // If rgb2hex exists, use it to convert; otherwise assume value is already a hex string
+    let color = (typeof this.rgb2hex === 'function') ? this.rgb2hex(value) : value;
+    // Set the input value to the color and update the preview icon background
+    $('input', this.element).val(color);
+    $('i', this.element).css('background-color', color);
+  },
 
-	setValue: function(value) {
-		let color = this.rgb2hex(value);
-		$('input', this.element).val();
-		$('i', this.element).css('background-color', value);
-	},
-	
-	init: function(data) {
-		let colorinput = this.render("bootstrap-color-picker-input", data);
-		let colorpicker = $('.input-group', colorinput).colorpicker();
-		return colorinput;
-	},
+  init: function(data) {
+    let colorinput = this.render("bootstrap-color-picker-input", data);
+    // initialize bootstrap colorpicker on the input-group inside the rendered element
+    $('.input-group', colorinput).colorpicker();
+    return colorinput;
   }
-);
-
+});
